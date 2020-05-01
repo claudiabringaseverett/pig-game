4e4d310a6117f6d02387bd3429aadbc565f64182
Claudia Bringas-Everett <claudiaeverett9@gmail.com>
2020-04-30 22:16:07 -0400

    adding rolling dice function

--- script.js
+++ script.js
@@ -19,17 +19,36 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice =  Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //Random Number
+    var dice =  Math.floor(Math.random() * 6) + 1;
+
+    //display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+
+    //update the roundscore if the rolled number was not a 1
+
+
+
+});
 
 
 
